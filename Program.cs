2abeff25a770949296cdbcfbf43e1efb9576655a
Your Name <you@example.com>
2016-03-02 10:20:35 -0500

    a.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
